e3415d49712e416493a294be5d1f1528da222496
Mahzabin <mashrufa1997@yahoo.co.uk>
2023-04-30 21:16:52 +0100

    wrote save, select_all and select functions in the 3 repositories

---
+++ repositories/category_repository.py
@@ -0,0 +1,32 @@
+from db.run_sql import run_sql
+from models.category import Category
+from models.customer_transaction import CustomerTransaction
+from models.merchant import Merchant
+
+def save(category):
+    sql = "INSERT INTO categories(name) VALUES (%s) RETURNING id"
+    values = [category.name]
+    results = run_sql( sql, values )
+    category.id = results[0]['id']
+    return category
+
+def select_all():
+    categories = []
+
+    sql = "SELECT * FROM categories"
+    results = run_sql(sql)
+
+    for row in results:
+        category = Category(row['name'], row['id'])
+        categories.append(category)
+    return categories
+
+def select(id):
+    category = None
+    sql = "SELECT * FROM categories WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        category = Category(result['name'], result['id'])
+    return category
---
+++ repositories/merchant_repository.py
@@ -0,0 +1,33 @@
+from db.run_sql import run_sql
+from models.category import Category
+from models.merchant import Merchant
+from models.customer_transaction import CustomerTransaction
+
+def save(merchant):
+    sql = "INSERT INTO merchants (name) VALUES (%s) RETURNING id"
+    values = [merchant.name]
+    results = run_sql(sql, values)
+    merchant.id = results[0]['id']
+    return merchant
+
+def select_all():
+    merchants = []
+
+    sql = "SELECT * FROM merchants"
+    results = run_sql(sql)
+
+    for row in results:
+        merchant = Merchant(row['name'], row['id'])
+        merchants.append(merchant)
+    return merchants
+
+def select(id):
+    merchant = None
+    sql = "SELECT * FROM merchants WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+
+    if results:
+        result = results[0]
+        merchant = Merchant(result['name'], result['id'])
+    return merchant
